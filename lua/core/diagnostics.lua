117beb5f7e27efecade5c2429365912559af01f5
yushi <yusufshihata2006@gmail.com>
2025-09-02 19:01:13 +0300

    silence nvim shit

--- lua/core/diagnostics.lua
+++ lua/core/diagnostics.lua
@@ -1,12 +1,6 @@
--- lua/core/diagnostics.lua
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
 vim.diagnostic.config({
-    virtual_text = false, -- cleaner look
-    float = { border = "rounded" },
+  virtual_text = false,
+  signs = false,
+  underline = false,
 })
 
